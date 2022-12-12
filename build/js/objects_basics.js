c83dfb0b30221ad3457846add4fbb00568f7ed65
Junaidhkn <junaidhkniazi@gmail.com>
2022-12-13 02:14:39 +0500

    commit

--- build/js/objects_basics.js
+++ build/js/objects_basics.js
@@ -1,1 +1,7 @@
 "use strict";
+let stringArr = ['one', 'hello', 'me'];
+let strNumArr = ['guitar', 'flute', 2133];
+let mixedData = ['ENF', 233, true];
+// OR
+// But still Types can't be redeclared even if 'LET'
+// let mixedData: (string | number | boolean)[] = ['ENF', 233, true];
---
+++ src/objects_basics.ts
@@ -0,0 +1,9 @@
+let stringArr: string[] = ['one', 'hello', 'me'];
+
+let strNumArr: (string | number)[] = ['guitar', 'flute', 2133];
+
+let mixedData: any[] = ['ENF', 233, true];
+
+// OR
+// But still Types can't be redeclared even if 'LET'
+// let mixedData: (string | number | boolean)[] = ['ENF', 233, true];
